f10f87c7651124b5a4416ba15466a9a01cd8ab46
j-rodrigueza-2018 <juanitorodrialonso@gmail.com>
2022-11-28 19:21:48 +0100

    Pop-up con datos del autor en la página de detalle del libro añadido

--- app/vistas/usuarios/detalleLibro.php
+++ app/vistas/usuarios/detalleLibro.php
@@ -18,11 +18,32 @@
             </div>
             <div class="col-lg-10 col-md-8 col-sm-12">
                 <p class="h5 col-12"><span class="subrayado">Título:</span> Título del Libro</p>
-                <p class="h5 col-12"><span class="subrayado">Autor:</span> Autor del Libro</p>
+                <p class="h5 col-12"><a class="text-black text-decoration-none" data-bs-toggle="modal" data-bs-target="#infoAutor"><span class="subrayado">Autor:</span> Autor del Libro</a></p>
                 <p class="h5 col-12"><span class="subrayado">Categoría:</span> Categoría del Libro</p>
                 <p class="h5 col-12"><span class="subrayado">Sinopsis:</span> Sinopsis del Libro</p>
             </div>
         </div>
+
+        <div class="modal fade" id="infoAutor" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Información del Autor</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p class="h6"><span class="subrayado">Nombre:</span> Nombre del autor.</p>
+                        <p class="h6"><span class="subrayado">Apellidos:</span> Apellidos del autor.</p>
+                        <p class="h6"><span class="subrayado">Fecha de Nacimiento:</span> Fecha de nacimiento del autor.</p>
+                        <p class="h6"><span class="subrayado">País de Origen:</span> País de origen del autor.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
     <?php include("includes/footer.php"); ?>
 </body>
