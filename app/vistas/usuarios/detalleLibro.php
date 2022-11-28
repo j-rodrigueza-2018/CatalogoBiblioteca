327f5e729e62c502d508e34a733783d074dd10b4
j-rodrigueza-2018 <juanitorodrialonso@gmail.com>
2022-11-28 18:29:12 +0100

    Página de detalle de libro añadida

---
+++ app/vistas/usuarios/detalleLibro.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca - Detalles del Libro</title>
+    <link rel="stylesheet" href="../../../public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../public/css/styles.css">
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+    <div class="container">
+        <p class="h1 mt-3 mb-3">DETALLES DEL LIBRO</p>
+        <div class="row">
+            <div class="col-lg-2 col-md-4 col-sm-12 mb-3">
+                <img src="../../../public/imagenesPortada/libro1.jpg" class="col-12 pe-2" width="200px">
+            </div>
+            <div class="col-lg-10 col-md-8 col-sm-12">
+                <p class="h5 col-12">Título: Título del Libro</p>
+                <p class="h5 col-12">Autor: Autor del Libro</p>
+                <p class="h5 col-12">Categoría: Categoría del Libro</p>
+                <p class="h5 col-12">Sinopsis: Sinopsis del Libro</p>
+            </div>
+        </div>
+    </div>
+    <?php include("includes/footer.php"); ?>
+</body>
+</html>
