9618283d0e96b54a0f90065e4d47e756a74afa65
j-rodrigueza-2018 <juanitorodrialonso@gmail.com>
2022-11-29 09:37:01 +0100

    Cambios en el listado de autores

--- app/vistas/gestor/listadoAutores.php
+++ app/vistas/gestor/listadoAutores.php
@@ -12,8 +12,18 @@
     <?php include("includes/header.php"); ?>
     <div class="container">
         <p class="h1 mt-3 mb-3">LISTADO DE AUTORES</p>
-        <button type="button" class="btn btn-success mb-3" onclick="location.href='formularioAutores.php'">+</button>
-        <button type="button" class="btn btn-danger mb-3"><img src="../../../public/img/papelera-de-reciclaje.png" width="20px" height="20px"></button>
+        <div class="row">
+            <div class="col-3">
+                <button type="button" class="btn btn-success mb-3" onclick="location.href='formularioAutores.php'">+</button>
+                <button type="button" class="btn btn-danger mb-3"><img src="../../../public/img/papelera-de-reciclaje.png" width="15px" height="20px"></button>
+            </div>
+            <div class="col-9">
+                <form class="d-flex ms-auto col-lg-6 col-md-6 col-sm-6" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Buscar por Apellido" aria-label="Buscar">
+                    <button class="btn btn-danger" type="submit">Buscar</button>
+                </form>
+            </div>
+        </div>
         <table class="table table-bordered col-12">
             <thead>
             <tr>
@@ -21,6 +31,7 @@
                 <th scope="col" class="text-center">Apellidos</th>
                 <th scope="col" class="text-center">Fecha de Nacimiento</th>
                 <th scope="col" class="text-center">País de Origen</th>
+                <th scope="col" class="text-center">Acciones</th>
             </tr>
             </thead>
             <tbody>
