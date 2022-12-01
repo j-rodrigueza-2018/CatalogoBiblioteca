e779e4747ff5a6af22a04829014bfc749ba95052
j-rodrigueza-2018 <juanitorodrialonso@gmail.com>
2022-12-01 13:18:39 +0100

    Fichero de configuración añadido

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Constantes para la conexión con la Base de Datos
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const NAME = "catalogoBiblioteca";
+
+// URL para acceder a la carpeta 'app'
+define("RUTA_APP", dirname(__FILE__, 2));
+
+// URL para acceder a la carpeta 'public'
+const RUTA_PUBLIC = "http://localhost/CatalogoBiblioteca";
+
+// Definimos el nombre de nuestra Aplicación
+const NOMBRE_APP = "Biblioteca";
