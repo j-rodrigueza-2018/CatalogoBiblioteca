91a1cebe21f4f3d72304df1bc562a024ad6d5378
j-rodrigueza-2018 <juanitorodrialonso@gmail.com>
2022-12-01 13:59:26 +0100

    Fichero para el tratamiento de la base de datos agregado

---
+++ app/librerias/Conexion.php
@@ -0,0 +1,39 @@
+<?php
+
+class Conexion {
+
+    // Atributos
+    private string $host = HOST;
+    private string $user = USER;
+    private string $pass = PASS;
+    private string $dbname = NAME;
+    protected array $rows = array();
+    private mysqli $dbh;
+
+    // Método Constructor
+    public function __construct() {
+        try {
+            $this->dbh = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+        } catch (mysqli_sql_exception $e) {
+            $this->error = $e->getMessage();
+        }
+    }
+
+    // Métodos públicos
+    public function query($query) {
+        return $this->dbh->query($query);
+    }
+
+    public function result_query($query): array {
+        $result = $this->dbh->query($query);
+        $num = mysqli_num_rows($result);
+        while ($row = $result->fetch_object()) {
+            $rows[] = $row;
+        }
+        if ($num == 0) {
+            $rows = array();
+        }
+        return $rows;
+    }
+
+}
